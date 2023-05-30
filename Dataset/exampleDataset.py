eab3a8160ee9b96d1e5c62ae4d88fa2682748ede
Great <greatattai442442@gmail.com>
2023-05-30 15:09:09 +0100

    Final extension problem fixed

---
+++ Dataset/exampleDataset.py
@@ -0,0 +1,74 @@
+dataset = [
+    {
+        "user_id": 1,
+        "browser_history": [
+            {
+                "timestamp": "2022-01-01 10:00:00",
+                "url": "https://example.com/page1",
+                "page_title": "Page 1",
+                "visit_frequency": 5
+            },
+            {
+                "timestamp": "2022-01-01 12:00:00",
+                "url": "https://example.com/page2",
+                "page_title": "Page 2",
+                "visit_frequency": 3
+            },
+            {
+                "timestamp": "2022-01-02 09:00:00",
+                "url": "https://example.com/page3",
+                "page_title": "Page 3",
+                "visit_frequency": 2
+            }
+        ],
+        "search_results": [
+            {
+                "title": "Machine Learning Algorithms",
+                "authors": ["John Smith", "Jane Doe"],
+                "citation_count": 120,
+                "publication_date": "2021-12-15",
+                "abstract": "..."
+            },
+            {
+                "title": "Deep Learning Techniques",
+                "authors": ["Alice Johnson", "Bob Williams"],
+                "citation_count": 95,
+                "publication_date": "2022-01-05",
+                "abstract": "..."
+            }
+        ]
+    },
+    {
+        "user_id": 2,
+        "browser_history": [
+            {
+                "timestamp": "2022-01-01 11:00:00",
+                "url": "https://example.com/page2",
+                "page_title": "Page 2",
+                "visit_frequency": 4
+            },
+            {
+                "timestamp": "2022-01-02 13:00:00",
+                "url": "https://example.com/page4",
+                "page_title": "Page 4",
+                "visit_frequency": 2
+            }
+        ],
+        "search_results": [
+            {
+                "title": "Introduction to Data Science",
+                "authors": ["Emily Johnson", "Mark Wilson"],
+                "citation_count": 80,
+                "publication_date": "2022-01-10",
+                "abstract": "..."
+            },
+            {
+                "title": "Statistical Analysis Methods",
+                "authors": ["David Brown", "Sarah Miller"],
+                "citation_count": 65,
+                "publication_date": "2021-12-20",
+                "abstract": "..."
+            }
+        ]
+    }
+]
